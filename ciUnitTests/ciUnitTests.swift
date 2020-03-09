a47f8fc1e2bedb946ed4f4812f7306db15943a55
Po Sam <po@mobairu.nl>
2020-03-09 19:44:16 +0100

    add fastlane file

---
+++ ciUnitTests/ciUnitTests.swift
@@ -0,0 +1,26 @@
+//
+//  ciUnitTests.swift
+//  ciUnitTests
+//
+//  Created by Po Sam on 04/03/2020.
+//  Copyright © 2020 Po Sam. All rights reserved.
+//
+
+import XCTest
+
+class ciUnitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
